93e4677ad175dc5e5b9e4ef4e3d1e52d1d229c42
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2022-12-29 22:30:22 +0330

    adding animation for navigation bar and offcanvas

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ gsap.registerPlugin(ScrollTrigger);
 
 let sections = gsap.utils.toArray(".section");
 
+
 gsap.to(sections, {
     xPercent: -100 * (sections.length - 1),
     ease: "none", 
@@ -12,4 +13,52 @@ gsap.to(sections, {
         snap: 1 / (sections.length - 1),
         end: "+=3500",
     }
+})
+
+const BURGER__MENU = document.querySelector('.burger__menu');
+const EXIT__BTN = document.querySelector('.exit__btn');
+
+let offcanvasRise = gsap.to(".divide__container", {
+    top: 0,
+    duration: 3,
+    ease: "power4.out",
+}).pause();
+
+var offcanvasAnime = gsap.to(".custom-shape-divider-bottom", {
+    keyframes: {
+        left: [0 , "-50%", "-30%", "-60%", "-100%","-80%", "-30%", "-50%", 0],
+    },
+    duration: 40,
+    repeat: -1,
+}).pause();
+
+BURGER__MENU.addEventListener('click', () => {
+    if(offcanvasRise.paused() || offcanvasRise.reversed()){
+        offcanvasRise.restart().delay(1);
+        offcanvasAnime.restart().delay(2);
+    }
+});
+
+EXIT__BTN.addEventListener('click', () => {
+    offcanvasAnime.pause();
+    offcanvasRise.reverse().delay(1);
+});
+
+// links hover animation 
+
+let links = document.querySelectorAll(".header .links"), hoverAnime;
+
+links.forEach((element) => {
+    element.addEventListener("mousemove", () => {
+        if(hoverAnime && hoverAnime.isActive()) return;
+        let children = element.querySelectorAll("span");
+        hoverAnime = gsap.to(children, {
+            keyframes: {
+                y: [0, -30, 0]
+            },
+            stagger: .05,
+            duration: .5,
+            ease: "power4.out"
+        })
+    })
 })
